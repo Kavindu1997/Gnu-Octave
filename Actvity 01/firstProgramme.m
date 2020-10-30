8582146eeaebd73ee597989d3eeaff6d453be7e7
Kavindu1997 <56603291+Kavindu1997@users.noreply.github.com>
2020-10-30 22:17:29 +0530

    Add files via upload
    Activity 01 on Octave

---
+++ Actvity 01/firstProgramme.m
@@ -0,0 +1,4 @@
+str1='Hello World !';
+str2='I am going to learn OCTAVE !';
+disp(str1)
+disp(str2)
---
+++ Actvity 01/startTime.m
@@ -0,0 +1,5 @@
+start=clock();
+size(start)  # row vector
+help clock   % Return the current local date and time as a date vector.
+
+startString=datestr(start)
