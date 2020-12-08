7afc4cc6afca0dc367cf2f560f05ab6f7067a292
Kavindu1997 <56603291+Kavindu1997@users.noreply.github.com>
2020-12-09 01:24:01 +0530

    reflection

---
+++ Activity 05/linearT.m
@@ -0,0 +1,27 @@
+%D=[1 1 3 3 2 1 3;2 0 0 2 3 2 2];
+%x=D(1,:);
+%y=D(2,:);
+%plot(x,y)
+
+
+
+%D=[1 1 3 3 2 1 3;2 0 0 2 3 2 2];
+%theta1=90*pi/180;
+%r1=[cos(theta1) -sin(theta1);sin(theta1) cos(theta1)];
+%rd1=r1*D
+%x=rd1(1,:);
+%y=rd1(2,:);
+%plot(x,y,'bo-')
+
+
+D=[1 1 3 3 2 1 3;2 0 0 2 3 2 2];
+R=[0 1;1 0];
+RD=R*D;
+x=RD(1,:);
+y=RD(2,:);
+hold on;
+plot(x,y,'ob-')
+axis([-1 4 -1 4],'equal');
+grid on;
+legend('original','reflected')
+hold off;
