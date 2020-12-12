99da5dbb59c020f621dc8c6cf338c00d158fe2da
Kavindu1997 <56603291+Kavindu1997@users.noreply.github.com>
2020-12-12 17:22:22 +0530

    string operations

--- Activity 06/practice.m
+++ Activity 06/practice.m
@@ -7,3 +7,34 @@ disp('\n')
 disp("samaraweera")
 disp("\n")
 disp("hello")
+
+stringconcat1=['foo' 'bar' 'huz']
+stingconcat2=['foo';'bar','huz']
+question=char('hi','dear','am','kavindu')
+answer=cstrcat('nice','to','meet','you') 
+
+matstr=mat2str([2 i;-i -2])
+intmat=int2str([1.35 1.5i;-1.5i 2.6])
+logicalstr=mat2str(logical(eye(3)))
+
+strGNU = "GNU\'s Not UNIX"
+spaces = (strGNU == ' ')
+strcmp(strGNU,'GNU') %0-not found , 1-found
+strncmp(strGNU,'GNx',3)
+strncmpi(strGNU,'gnu',3)
+
+quote ="First things first, but not necessarily in that order"
+quote( quote == " " ) = "_"
+
+
+deblank("  string  ")
+strtrim(["       d                 d"])
+strtrunc("12456789",5)
+
+index("kavindu kavishanka",'kavi','last')
+strfind('abababa', 'aba')
+
+
+str = 'The staff email is xyz@ucsc.cmb.ac.lk'
+pat = '([\w-.])+@([\w-.])+'
+email = regexp(str, pat, 'match')
