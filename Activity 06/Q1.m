b590d10a42f3eab81b52e7e0e608b3f139f1e3c4
Kavindu1997 <56603291+Kavindu1997@users.noreply.github.com>
2020-12-13 11:50:39 +0530

    Exercise 1

---
+++ Activity 06/Q1.m
@@ -0,0 +1,14 @@
+stringconcat=['foo';'bar',"huz"]
+size(stringconcat)
+
+myMovie="I\'m number four"
+
+mat=["| 1 2 3 |";"| 4 5 6 |";"| 7 8 9 |"]
+
+
+data=[" a ";"b ";" c"];
+deblank(data)
+strtrim(data)
+
+string="kahatagasdigiliya"
+substr(string,-8,4)
